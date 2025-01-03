cb698c2ecdc7f131868b545d82550951968f085b
zebz13 <zeb@fedora>
2025-01-03 13:56:31 +0530

    Updating logic for a>=(total-a)- editorial

--- 2270.py
+++ 2270.py
@@ -1,23 +1,18 @@
 from functools import lru_cache
 class Solution:
     def waysToSplitArray(self, nums: list[int]) -> int:
-        left_prefix = [0]*len(nums)
-        right_prefix = [0]*len(nums)
-        sum1 = 0
-        sum2 = 0
-        for index in range(len(nums)):
-            sum1+=nums[index]
-            sum2+=nums[len(nums)-1-index]
-            left_prefix[index] = sum1
-            right_prefix[len(nums)-1-index] = sum2
-        
+        left_sum = 0
+        right_sum = sum(nums)
+
         @lru_cache()
         def solve():
             valid_couhnt = 0
-            for index in range(1,len(nums)):
-                if(left_prefix[index-1]>=right_prefix[index]):
+            nonlocal left_sum, right_sum
+            for index in range(0,len(nums)-1):
+                left_sum+=nums[index]
+                right_sum-=nums[index]
+                if left_sum>=right_sum:
                     valid_couhnt+=1
-
             return valid_couhnt
         return solve()
     
